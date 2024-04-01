b9459aff477520e0e8f7f29bbf8c634a16027ccd
RAHUL VR <iamrahulvr29@gmail.com>
2024-04-01 13:19:17 +0530

    correction update

--- application/views/user/create.php
+++ application/views/user/create.php
@@ -14,38 +14,41 @@
     </style>
 </head>
 <body>
-    <div class="container">
+    <div class="container mt-5">
         <h2>Add User</h2>
         <?php echo form_open('user/store'); ?>
             <div class="form-group">
                 <label>Username:</label>
-                <input class="form-control" type="text" name="username">
+                <input class="form-control" type="text" name="username" required >
             </div>
             <div class="form-group">
                 <label>Email:</label>
-                <input class="form-control" type="email" name="email">
+                <input class="form-control" type="email" name="email" required>
             </div>
             <div class="form-group">
                 <label>Mobile:</label>
-                <input class="form-control" type="number" name="mobile">
+                <input class="form-control" type="number" name="mobile" required>
             </div>
             <div class="form-group">
                 <label>Age:</label>
-                <input class="form-control" type="number" name="age">
+                <input class="form-control" type="number" name="age" required>
             </div>
             <div class="form-group">
                 <label>Gender:</label>
-                <input class="form-control" type="text" name="gender">
+                <input class="form-control" type="text" name="gender" required>
             </div>
             <div class="form-group">
                 <label>Password:</label>
-                <input class="form-control" type="password" name="password">
+                <input class="form-control" type="password" name="password" required>
             </div>
             <div class='d-flex'>
-            <button class="btn btn-primary" type="submit">Add User</button>
-            <div class='px-3'> <a href="index.php"><button class="btn btn-danger px-3">Cancel</button></a></div>
+                <div>
+                    <button class="btn btn-primary" type="submit">Add User</button>
+                </div>
+                <div class='px-3'> 
+                    <a href="index" class="btn btn-danger px-3">Cancel</a>
+                </div>
             </div>
-
         <?php echo form_close(); ?>
     </div>
 
